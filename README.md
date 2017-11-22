2f5671c0b563d8e6f4fa2a96a7e7e1d185303157
Ajay Singh <ajaysimple91@gmail.com>
2017-11-23 01:58:47 +0530

    code implemented

--- README.md
+++ README.md
@@ -3,11 +3,38 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+A: My first approach was to find all the duplicates from the grid and then look for the twins in the grid. For each twin box, I was trying to remove the match character(character which were present in both peers and twins) from peers.
+	
+	dups = dict()
+    for key, value in values.items():
+        if len(value) == 2:
+            if value not in dups:
+                dups[value] = [key]
+            else:
+                dups[value].append(key)
+    
+    twins = dict()
+    for key,value in dups.items():
+        if len(value) == 2:
+            if not key in twins:
+                twins[key] = value
+            else:
+                twins[key].extend(value) 
+
+    for key in twins:
+        for box in twins[key]:
+            print(peers[box])
+            for peer in peers[box]:
+                for key_element in list(key):
+                    if values[peer] != key and len(values[peer])>=2:
+                        assign_value(values, peer, values[peer].replace(key_element, ''))
+
+ But after looking the explaination on http://www.sudokudragon.com/tutorialnakedtwins.htm, I got to know my the fault in my approach.
+ So, I changed my approach to remove matching character from either row_units and square units or column_units and square_units as twins will make impact on either row or column but not on both at the same time.
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
+A: I just create the diagonal units and add it to the unitlist. Units are being to used to create peers list to make sure that numbers are not repeated in any unit.
 
 ### Install
 
@@ -24,21 +51,21 @@ If not, please see how to download pygame [here](http://www.pygame.org/download.
 
 ### Code
 
-* `solution.py` - Fill in the required functions in this file to complete the project.
-* `test_solution.py` - You can test your solution by running `python -m unittest`.
-* `PySudoku.py` - This is code for visualizing your solution.
-* `visualize.py` - This is code for visualizing your solution.
+* `solution.py` - You'll fill this in as part of your solution.
+* `solution_test.py` - Do not modify this. You can test your solution by running `python solution_test.py`.
+* `PySudoku.py` - Do not modify this. This is code for visualizing your solution.
+* `visualize.py` - Do not modify this. This is code for visualizing your solution.
 
 ### Visualizing
 
-To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
+To visualize your solution, please only assign values to the values_dict using the ```assign_values``` function provided in solution.py
 
 ### Submission
 Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
 
 The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
 
-To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
+To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login for alternate login instructions.
 
 This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
 
--- solution.py
+++ solution.py
@@ -1,4 +1,25 @@
 assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+# leading diagonal
+diagonal_units1 = [[x+y for x, y in zip(rows, cols)]]
+#counter-diagonal
+diagonal_units2 = [[x+y for x, y in zip(rows, cols[::-1])]]
+# diagonal units to make sure the no duplicate on diagonal
+diagonal_units = diagonal_units1 + diagonal_units2
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def assign_value(values, box, value):
     """
@@ -27,9 +48,39 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    # dictionary for naked twins with 'value'of grid as key 
+    twins = dict()
+    for unit in unitlist:
+        # dictionary to find possible twins with 'value' of grid as key
+        dups = dict()
+        # loop to find  possible twins
+        for box in unit:
+            if len(values[box]) == 2:
+                if values[box] not in dups:
+                    dups[values[box]] = [box]
+                else:
+                    dups[values[box]].append(box)
+        # make sure the twins are naked twins
+        for key,value in dups.items():
+            if len(value) == 2:
+                if key not in twins:
+                    twins[key] = [unit]
+                else:
+                    twins[key].append(unit)
+    # remove the matched character as per the units.
+    for key in twins:
+        for unit in twins[key]:
+            for box in unit:
+                for key_element in list(key):
+                    if values[box] != key and len(values[box])>=2:
+                        assign_value(values, box, values[box].replace(key_element, ''))
+
+
+
+    return values
+
+
+
 
 def grid_values(grid):
     """
@@ -41,7 +92,12 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    sudoku_board = dict( zip( boxes, grid))
+    d = '123456789'
+    for key in sudoku_board:
+        if sudoku_board[key] is '.':
+            assign_value(sudoku_board, key, d)
+    return sudoku_board
 
 def display(values):
     """
@@ -49,19 +105,76 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    for key in values:
+        if len(values[key]) is 1:
+            for peer in peers[key]:
+                 assign_value(values, peer, values[peer].replace(values[key], ''))
+    
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for d in '123456789':
+            exclusive = [square for square in unit if d in values[square]]
+            if len(exclusive) is 1:
+                assign_value(values, exclusive[0], d)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # use naked twins Strategy
+        values = naked_twins(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    # Choose one of the unfilled squares with the fewest possibilities
+    
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+
+    # If you're stuck, see the solution.py tab!
+    
+    if values is False:
+        return False
+    if all(len(values[s]) == 1 for s in boxes):
+        return values
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,6 +185,9 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    values = search(values)
+    return values
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
@@ -85,3 +201,5 @@ if __name__ == '__main__':
         pass
     except:
         print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+
+
